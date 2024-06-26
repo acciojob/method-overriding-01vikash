5a4608d981fd44204c2ceef02a3025558fb67262
sudoku-DSA <vikashverma10102001@gmail.com>
2024-06-26 09:26:31 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        A a = new A();
+        String s =a.meth();
+        System.out.println(s);
+        B b=new B();
+         s=b.meth();
+        System.out.println(s);
+    }
+
+    public static class B extends A{
+        public String meth(){
+           return  "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
 }
